6fdde61ff90ada19a4c01adec8419d4374c7425a
argos-panoptes <github.iav@aleeas.com>
2023-02-20 09:22:27 +0800

    Set up

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Instructions:
-// 1) Log in to AISIS and open the Course and Faculty Evaluation page
-// 2) Navigate to the specific professor you wish to rate
-// 3) Press F12 or right-click and click "Inspect Element"
-// 4) Navigate to the "Console" tab
-// 5) Paste the whole text into the text field
-// 6) Follow instructions on screen
-// NOTE: You can only type "Areneyow()" into the "Console" tab when answering another professor's evaluation. If you reload/refresh the page, you may need to re-do the instructions again.
- 
 function Areneyow() {
     radioIDs = Array.from(new Array(40), (x, i) => i + 153);
     radioIDs = radioIDs.map(i => 'Q' + i);
